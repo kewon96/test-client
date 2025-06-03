0bdd17b7cd6120d664c1cb998ac5aaf28f30463e
Seung Hyeon <yang0184@naver.com>
2025-06-04 02:41:10 +0900

    send

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
+import axios from "axios";
+const BASE_URL = 'https://api-dev.ubittz.co.kr/i-planning';
+
 const stompClient = new StompJs.Client({
-  brokerURL: 'ws://localhost:8080/gs-guide-websocket'
+  // brokerURL: 'ws://localhost:8080/gs-guide-websocket'
+  // brokerURL: 'http://localhost:8282/i-planning/ws-chat'
+  brokerURL: `${BASE_URL}/ws-chat`
 });
 
 stompClient.onConnect = (frame) => {
   setConnected(true);
   console.log('Connected: ' + frame);
-  stompClient.subscribe('/topic/greetings', (greeting) => {
+  stompClient.subscribe('/topic/room/room-1', (greeting) => {
+      console.log(greeting);
       showGreeting(JSON.parse(greeting.body).content);
   });
 };
@@ -42,10 +48,27 @@ function disconnect() {
 }
 
 function sendName() {
-  stompClient.publish({
-      destination: "/app/hello",
-      body: JSON.stringify({'name': $("#name").val()})
-  });
+  console.log();
+
+  // const axios = require('axios').defaults;
+  // const axios = import 'axios'
+
+  const message = {
+    type: 'CHAT',
+    context: "내용내용",
+    sender: '사용자1',
+    roomId: "room-1",
+  };
+
+  axios.put(`${BASE_URL}/chat/send`, message);
+  
+  
+  // stompClient.publish({
+  //     // destination: "/app/hello",
+  //     destination: "/chat.sendMessageToRoom",
+  //     // body: JSON.stringify({'name': $("#name").val()})
+  //     body: JSON.stringify(message)
+  // });
 }
 
 function showGreeting(message) {
